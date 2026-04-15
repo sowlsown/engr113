c1ee3a35e48b1d63eaabb71f50afb5cf55b98896
qorie <145498398+qorie@users.noreply.github.com>
2026-04-15 15:02:39 -0400

    Update Lab_3_Huyen.py

--- Lab_3_Huyen.py
+++ Lab_3_Huyen.py
@@ -9,7 +9,7 @@ from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
 from irobot_edu_sdk.music import Note
 
 robot = Create3(Bluetooth())
-speed = 10
+speed = 25
 th = 150
 flag = 0
 
@@ -25,8 +25,10 @@ async def touched(robot):
 async def play(robot):
     n_s = 0
     await robot.reset_navigation()  # get original pos
-    
+
     while True:    
+        print(n_s)
+
         sensors = (await robot.get_ir_proximity()).sensors
 
         if n_s == 0: #robot begins to move straight forward at speed assigned            
@@ -35,60 +37,73 @@ async def play(robot):
         
         elif n_s == 1: #continues until meets wall, then turn right and  
             if sensors[3] > th:
-                await robot.move(-5)
+                await robot.set_wheel_speeds(0, 0)
+                # await robot.move(-2)
                 await robot.turn_right(90)
+                await robot.set_wheel_speeds(speed, speed)
+
                 n_s = 2
                     
         elif n_s == 2: #moves straight along the wall until sensor can no longer see wall
-            await robot.set_wheel_speeds(speed, speed)
             if sensors[0] < th: #can't sense wall anymore
-                await robot.move(20)
-                await robot.turn_left()
-                firs_open = await robot.get_position()
+                await robot.set_wheel_speeds(0, 0)
+                y1 = (await robot.get_position()).y
+                await robot.move(25)
+                await robot.turn_left(90)
+                x1 = (await robot.get_position()).x
+                await robot.set_wheel_speeds(speed, speed)
                 n_s = 3
 
         elif n_s == 3: # moves forward until next wall
-            await robot.set_wheel_speeds(speed, speed)
-            n_s = 4
+            
+            if sensors[3] > th:
+                await robot.set_wheel_speeds(0, 0)
+                await robot.turn_left(90)
+                await robot.set_wheel_speeds(speed, speed)
+                n_s = 4
         
         elif n_s == 4:
-            if sensors[3] > th:
-                await robot.move(-5)
-                await robot.turn_left(90)
+            if sensors[6] < th: #can't sense wall anymore
+                await robot.set_wheel_speeds(0, 0)
+                await robot.move(25)
+                await robot.turn_right(90)
+                pos2 = await robot.get_position() #position of second opening
                 n_s = 5
         
-        elif n_s == 5:
-            await robot.set_wheel_speeds(speed, speed)
-            if sensors[6] < th: #can't sense wall anymore
-                await robot.move(20)
-                await robot.turn_right()
-                sec_open = await robot.get_position() #position of second opening
-                n_s = 6
-        
-        elif n_s == 6: 
-            await robot.move(200 - sec_open.y)
-            n_s = 7
+        elif n_s == 5: 
+            await robot.move(200 - pos2.y)
+            n_s = 6
 
-        elif n_s == 7:
+        elif n_s == 6:
             if flag == 1:
-                n_s = 8
+                n_s = 7
         
-        elif n_s == 8:
-            # moves to first opening
-            await robot.move(firs_open.y)
+        elif n_s == 7:
+            #moves to first opening
             await robot.turn_right(90)
-            await robot.move(firs_open.x)
+            await robot.move(y1)
             await robot.turn_left(90)
+            await robot.move(x1)
             # moves to second opening
-            await robot.move(sec_open.y - firs_open.y)
+            await robot.move(pos2.y - y1)
             await robot.turn_left(90)
             temper = await robot.get_position()
-            await robot.move(temper.x - sec_open.x)
+            await robot.move(temper.y - pos2.y)
             await robot.turn_right(90)
             # move to the end
-            await robot.move(200 - sec_open.y)
+            await robot.move(200 - pos2.x)
+            
+            # await robot.reset_navigation()
+            # await robot.navigate_to(firs_open.y, 0, 90)
+            # await robot.turn_right(90)
+            # await robot.navigate_to(0, firs_open.x, 90)
+            # await robot.navigate_to(sec_open.y, 0, 90)
+            # await robot.turn_left(90)
+            # await robot.navigate_to(0, sec_open.x, 90)
+            # await robot.move(50)
+            n_s = 8
 
-        elif n_s == 9:
+        elif n_s == 8:
             await robot.set_lights_on_rgb(255, 255, 0) 
 
 
