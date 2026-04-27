5320a6c3a6c1c241e14b92e1bf10386c0bee46e9
Andrew Pomar <agp74@drexel.edu>
2026-04-27 13:57:01 -0400

    Bug Fix: Should have fixed the weird position stuff happening.

--- Lab_3.py
+++ Lab_3.py
@@ -9,92 +9,95 @@ from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
 from irobot_edu_sdk.music import Note
 
 robot = Create3(Bluetooth())
-speed = 10
+speed = 25
 th = 150
+flag = 0
 
+## Rerun Press left button ## 
+@event(robot.when_touched, [True, False])  # (.) button. 
+async def touched(robot): 
+    global flag 
+    await robot.set_lights_on_rgb(0, 0, 255) 
+    flag = 1
 
-async def forward(robot):
-    await robot.set_lights_on_rgb(0, 255, 0)
-    await robot.set_wheel_speeds(speed, speed)
-
-
-async def backoff_right(robot):
-    await robot.set_lights_on_rgb(255, 80, 0)
-    await robot.move(-20)
-    await robot.turn_right(90)
-
-async def backoff_left(robot):
-    await robot.move(-20)
-    await robot.turn_left(90)
-
-
-def front_obstacle(sensors):
-    print(sensors[3])
-    return sensors[3] > th
-
-def left_obstacle(sensors):
-    return sensors[0] > th
-
-def right_obstacle(sensors):
-    return sensors[6] > th
 
 @event(robot.when_play)
 async def play(robot):
-    
     n_s = 0
-    await robot.get_position() # get original pos
-    await forward(robot)
-    while True:
-        
+    await robot.reset_navigation()  # get original pos
+
+    while True:    
+        print(n_s)
+
         sensors = (await robot.get_ir_proximity()).sensors
-        if n_s == 0: #detects if theres a wall, backs off, turns, and then stops
+
+        if n_s == 0: #robot begins to move straight forward at speed assigned            
+            await robot.set_wheel_speeds(speed, speed)
+            n_s = 1
         
-            if front_obstacle(sensors):
-                await backoff_right(robot)
+        elif n_s == 1: #continues until meets wall, then turn right and  
+            if sensors[3] > th:
                 await robot.set_wheel_speeds(0, 0)
-                n_s += 1
-        
-        elif n_s == 1: #will detect the wall, when theres no wall, it will turn left and stop
-            
-            if left_obstacle(sensors):
+                # await robot.move(-2)
+                await robot.turn_right(90)
                 await robot.set_wheel_speeds(speed, speed)
-                if(sensors[0] < th):
-                    await robot.move(30)
-                    await robot.turn_left(90)
-                    await robot.set_wheel_speeds(0, 0)
-                    opening1 = await robot.get_position() #get position of opening 1
-                    n_s = 2
+
+                n_s = 2
                     
-        elif n_s == 2: #it will move forward unless the robot detects there's something in front of it
-            
-            if (sensors[3] < th):
+        elif n_s == 2: #moves straight along the wall until sensor can no longer see wall
+            if sensors[0] < th: #can't sense wall anymore
+                await robot.set_wheel_speeds(0, 0)
+                await robot.move(25)
+                await robot.turn_left(90)
                 await robot.set_wheel_speeds(speed, speed)
-                if front_obstacle(sensors):
-                    await backoff_left(robot)
-                    await robot.set_wheel_speeds(0, 0)
-                    n_s = 3
-        
-        elif n_s == 3: #This finds the opening and orients itself
+                opening1 = await robot.get_position()
+                n_s = 3
+
+        elif n_s == 3: # wall 2
             
-            if right_obstacle(sensors):
+            if sensors[3] > th:
+                await robot.set_wheel_speeds(0, 0)
+                await robot.turn_left(90)
                 await robot.set_wheel_speeds(speed, speed)
-                if(sensors[6] < th):
-                    await robot.move(30)
-                    await robot.turn_right(90)
-                    await robot.set_wheel_speeds(0, 0)
-                    opening2 = await robot.get_position() #get pos of opening 2
-                    n_s = 4
+                
+                n_s = 4
         
-        elif n_s == 4: #this will drive through the opening with the position
-            
-            if left_obstacle(sensors) == True or right_obstacle(sensors) == True:
-                await robot.set_wheel_speeds(speed, speed)
-                if left_obstacle(sensors) == False or right_obstacle(sensors) == False:
-                    await robot.set_wheel_speeds(0, 0)
-                    Final_Pos = await robot.get_position()
-                    n_s = 5
+        elif n_s == 4: # opening 2
+            if sensors[6] < th: #can't sense wall anymore
+                await robot.set_wheel_speeds(0, 0)
+                await robot.move(25)
+                await robot.turn_right(90)
+                opening2 = await robot.get_position()
+                n_s = 5
         
-        elif n_s == 5: #Want to make it just go to that position
+        elif n_s == 5: 
+            await robot.move(20)
             
+            finalpos = await robot.get_position()
+            n_s = 6
+
+        elif n_s == 6:
+            if flag == 1:
+                await robot.reset_navigation()
+                n_s = 7
+        
+        elif n_s == 7: # rerun
+            
+            #first opening
+            await robot.navigate_to(opening1.x, 0, 90) #should move to first position's x
+            await robot.navigate_to(opening1.x, opening1.y, 90)
+            await robot.move(30)
+            finalpos1 = await robot.get_position()
+            
+            #second opening
+            await robot.navigate_to(opening2.x, finalpos1.y, 90)
+            await robot.navigate_to(opening2.x, finalpos.y, 90)
+            #should be in the final part
+            
+            n_s = 8
+
+        elif n_s == 8:
+            await robot.set_lights_on_rgb(255, 255, 0) 
+
 
 robot.play()
