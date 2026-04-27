99f4ac982d6abf14b94ab35c14ead3dd7e8db525
duypham1231234 <lumpychunkymonkey@gmail.com>
2026-04-27 14:19:55 -0400

    Update duy_lab4.py

--- duy_lab4.py
+++ duy_lab4.py
@@ -1,6 +1,6 @@
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
-from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
-from irobot_edu_sdk.music import Note
+from irobot_edu_sdk.robots import event, Color, Create3
+import asyncio
 
 robot = Create3(Bluetooth())
 
@@ -9,6 +9,7 @@ th = 150
 
 async def stop(robot):
     await robot.set_wheel_speeds(0, 0)
+    await robot.set_lights_on_rgb(255, 0, 0)
 
 async def forward(robot):
     await robot.set_lights_on_rgb(0, 255, 0)
@@ -16,44 +17,58 @@ async def forward(robot):
 
 @event(robot.when_play)
 async def play(robot):
+    state = 0   # 0 = searching, 1 = follow left, 2 = follow right
     await forward(robot)
-
-    state = 0   
-    # 0 = searching, 1 = follow left, 2 = follow right
 
     while True:
         sensors = (await robot.get_ir_proximity()).sensors
         left = sensors[0]
         right = sensors[6]
 
-
+        # -------------------------
+        # STATE 0: SEARCH FOR WALL
+        # -------------------------
         if state == 0:
             await forward(robot)
 
             if left > th:
-                state = 1   # follow left wall
+                state = 1
             elif right > th:
-                state = 2   # follow right wall
+                state = 2
 
-
+        # -------------------------
+        # STATE 1: FOLLOW LEFT WALL
+        # -------------------------
         elif state == 1:
             await forward(robot)
 
-            # lost the wall -> stop sequence
+            # lost wall
             if left < th:
                 await robot.move(40)
                 await robot.turn_left(90)
-                await forward(robot)
 
+                sensors = (await robot.get_ir_proximity()).sensors
+                if sensors[0] < th:
+                    await stop(robot)
+                    return
+                else:
+                    await forward(robot)
 
-
-
+        # -------------------------
+        # STATE 2: FOLLOW RIGHT WALL
+        # -------------------------
         elif state == 2:
             await forward(robot)
 
-            # lost the wall -> stop sequence
+            # lost wall
             if right < th:
                 await robot.move(40)
                 await robot.turn_right(90)
-                await forward(robot)
+
+                sensors = (await robot.get_ir_proximity()).sensors
+                if sensors[6] < th:
+                    await stop(robot)
+                    return
+                else:
+                    await forward(robot)
 
