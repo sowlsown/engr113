e82f5f59d5c7105c0d15c384e272624a42c0bf5e
qorie <145498398+qorie@users.noreply.github.com>
2026-05-28 16:11:48 -0400

    Update adictdid.py

--- adictdid.py
+++ adictdid.py
@@ -1,15 +1,15 @@
 #"key":(x,y, length, height)
 {
-  "Veggies":(5,16,1,2),
-  "Fruits":(11,16,1,2),
-  "Meats":(16,15,2,1),
-  "Pastries":(2,7,1,3),
+  "Veggies":(1,11,1,2),
+  "Fruits":(1,5,1,2),
+  "Meats":(2,1,2,1),
+  "Pastries":(2,8,1,3),
   "Condiments": (5,8,1,3),
   "Canned":(6,8,1,4),
-  "Meals":(7,15,2,1),
+  "Meals":(7,1,2,1),
   "Snacks":(10,8,1,4),
   "Cereal":(11,8,1,3),
   "Houseware":(14,8,1,3),
-  "Dairy":(13,15,2,1),
-  "Beverage":(15,10,1,3),
+  "Dairy":(13,1,2,1),
+  "Beverage":(15,6,1,3),
 }
